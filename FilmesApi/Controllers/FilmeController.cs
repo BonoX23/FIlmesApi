20814064e55e5799e13e43d8734caa2823a90589
Ricardo Bono <ricardo.bonos@gmail.com>
2024-03-22 16:17:58 -0300

    Consulta e paginação

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -8,16 +8,32 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
+
+    // A função CreatedAtAction retorna o que foi criado
     [HttpPost]
-
-    public void AdicionarFilme([FromBody] Filme filme)
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
-
-        
+        return CreatedAtAction(nameof(RecuperaFilmesporId), new { id = filme.Id }, filme);
     }
 
+    // Skip e take são para paginacao
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0,
+        [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    // IActionResult trás o resultado de uma ação que foi executada
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmesporId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
 }
