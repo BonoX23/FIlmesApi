b3423b6cb58178108d5b9605b19379f1bf2ed833
Ricardo Bono <ricardo.bonos@gmail.com>
2024-03-30 03:32:58 -0300

    Aplicando relacionamento

--- FilmesApi/Data/FilmeContext.cs
+++ FilmesApi/Data/FilmeContext.cs
@@ -8,6 +8,29 @@ public class FilmeContext : DbContext
     public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts)
     {
     }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Sessao>()
+            .HasKey(sessao => new { sessao.FilmeId, sessao.CinemaId });
+
+        //HasOne => Uma sessão tem um cinema
+        //Withmany => Um cinema tem varias sessões
+        //HasForeignkey => Chave estrangeira é o Id do Cinema
+        builder.Entity<Sessao>()
+            .HasOne(sessao => sessao.Cinema)
+            .WithMany(cinema => cinema.Sessoes)
+            .HasForeignKey(sessao => sessao.CinemaId);
+
+        builder.Entity<Sessao>()
+            .HasOne(sessao => sessao.Filme)
+            .WithMany(filme => filme.Sessoes)
+            .HasForeignKey(sessao => sessao.FilmeId);
+
+        builder.Entity<Endereco>()
+            .HasOne(endereco => endereco.Cinema)
+            .WithOne(cinema => cinema.Endereco)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 
     public DbSet<Filme> Filmes { get; set; }
     public DbSet<Cinema> Cinemas { get; set; }
